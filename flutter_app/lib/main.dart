e0af355dbe114558e5f60ff293b9da70ac824f4f
mirko <zhouchao180@gmail.com>
2019-06-11 01:42:08 +0800

    study StatefulWidget update UI

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -14,12 +14,14 @@ class _AppState extends State<App>{
 
   var pwdColor = Color.fromRGBO(8, 109, 202,1);
   var codeColor = Colors.white;
+  var isPwdLogin = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "demo",
       home: new Container(
+        width: 550.0,
         padding: EdgeInsets.fromLTRB(20.0, 120.0, 20.0, 50.0),
         decoration: BoxDecoration(
             image: DecorationImage(
@@ -41,10 +43,10 @@ class _AppState extends State<App>{
                     child: new Container(
                       height: 40.0,
                       child: new RaisedButton(
-                        onPressed: () => {},
+                        onPressed: _pwdChoose,
                         child: new Text("密码登陆"),
                         color: const Color.fromRGBO(111, 168, 223,1),
-                        textColor: Color.fromRGBO(8, 109, 202,1),
+                        textColor: pwdColor,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.only(topLeft: Radius.circular(5.0)),
                         ),
@@ -56,10 +58,10 @@ class _AppState extends State<App>{
                     child: new Container(
                       height: 40.0,
                       child: new RaisedButton(
-                        onPressed: () => {},
+                        onPressed: _codeChoose,
                         child: new Text("验证码登陆"),
                         color: const Color.fromRGBO(111, 168, 223,1),
-                        textColor: Colors.white,
+                        textColor: codeColor,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.only(topRight: Radius.circular(5.0))
                         ),
@@ -72,6 +74,10 @@ class _AppState extends State<App>{
             ),
             new Container(
               width: 250.0,
+              child: Divider(height:0.7,indent:0.0,color: Color.fromRGBO(111, 168, 223,1)),
+            ),
+            new Container(
+              width:250.0,
               height: 40.0,
               decoration: BoxDecoration(
                 color: Color.fromRGBO(88, 154, 219,1),
@@ -96,20 +102,49 @@ class _AppState extends State<App>{
                 ],
               ),
             ),
+            new Container(
+              width: 250.0,
+              child: Divider(height:0.7,indent:0.0,color: Color.fromRGBO(111, 168, 223,1)),
+            ),
+            new Container(
+              width: 250.0,
+              height: 40.0,
+              decoration: BoxDecoration(color: Color.fromRGBO(88, 154, 219,1)),
+              child: new Row(
+                children: <Widget>[
+                  new Container(
+                    width: 40.0,
+                    height: 40.0,
+                    padding: EdgeInsets.fromLTRB(10.0,10.0,10.0,10.0),
+                    child: new Image(
+                      image: AssetImage('images/tx.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
 
-    _pwdChoose() {
+  void _pwdChoose() {
+    setState(() {
       pwdColor = Color.fromRGBO(8, 109, 202,1);
       codeColor = Colors.white;
-    }
+      isPwdLogin = true;
+    });
+    print("方法已执行__");
+  }
 
-    _codeChoose() {
+  void _codeChoose() {
+    setState(() {
       pwdColor = Colors.white;
       codeColor = Color.fromRGBO(8, 109, 202,1);
-    }
+      isPwdLogin = false;
+    });
   }
 }
 
